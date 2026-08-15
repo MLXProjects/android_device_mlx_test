eb5421367b3bda32a3af548c73bfc35aa5798a61
MLXProjects <maicolinux4@gmail.com>
2026-08-15 01:15:54 +0000

    use prebuilt kernel

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,1 +1,14 @@
 # AndroidBoard.mk for mlx_test
+
+LOCAL_PATH := $(call my-dir)
+
+# Use the prebuilt kernel in this device tree.
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+endif
+
+# Copy the prebuilt kernel into the boot image.
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
